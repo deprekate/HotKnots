3e0b34b5dbb73dda1ff81cb3a1d642d93023980a
deprekate <deprekate@gmail.com>
2022-02-02 15:52:41 -0800

    Converting the external flat files into builtin variables

---
+++ dat/multirnafold.conf.h
@@ -0,0 +1,129 @@
+// configuration file
+
+// RNA thermodynamic parameters
+// files for stacked pairs
+RNA_STACK_ENERGY37_V31      =
+#include "turner-stack-rna.dat"
+RNA_STACK_ENERGY37_V23      =
+#include "turner-stack-rna.dg"
+RNA_STACK_ENTHALPY          =
+#include "turner-stack-rna.dh"
+// files for hairpin loops
+RNA_LOOP_ENERGY37_V31       =
+#include "turner-loop-rna.dat"
+RNA_LOOP_ENERGY37_V23       =
+#include "turner-loop-rna.dg"
+RNA_LOOP_ENTHALPY           =
+#include "turner-loop-rna.dh"
+RNA_TRILOOP_ENERGY37_V31    =
+#include "turner-triloop-rna.dat"
+RNA_TRILOOP_ENERGY37_V23    =
+#include "turner-triloop-rna.dg"
+RNA_TRILOOP_ENTHALPY        =
+#include "turner-triloop-rna.dh"
+RNA_TLOOP_ENERGY37_V31      =
+#include "turner-tloop-rna.dat"
+RNA_TLOOP_ENERGY37_V23      =
+#include "turner-tloop-rna.dg"
+RNA_TLOOP_ENTHALPY          =
+#include "turner-tloop-rna.dh"
+RNA_TSTACKH_ENERGY37_V31    =
+#include "turner-tstackh-rna.dat"
+RNA_TSTACKH_ENERGY37_V23    =
+#include "turner-tstackh-rna.dg"
+RNA_TSTACKH_ENTHALPY        =
+#include "turner-tstackh-rna.dh"
+
+// files for internal loops or bulges
+RNA_TSTACKI_ENERGY37_V31    =
+#include "turner-tstacki-rna.dat"
+RNA_TSTACKI_ENERGY37_V23    =
+#include "turner-tstacki-rna.dg"
+RNA_TSTACKI_ENTHALPY        =
+#include "turner-tstacki-rna.dh"
+RNA_INT11_ENERGY37_V31      =
+#include "turner-sint2-rna.dat"
+RNA_INT11_ENERGY37_V23      =
+#include "turner-sint2-rna.dg"
+RNA_INT11_ENTHALPY          =
+#include "turner-sint2-rna.dh"
+RNA_INT21_ENERGY37_V31      =
+#include "turner-asint1x2-rna.dat"
+RNA_INT21_ENERGY37_V23      =
+#include "turner-asint1x2-rna.dg"
+RNA_INT21_ENTHALPY          =
+#include "turner-asint1x2-rna.dh"
+RNA_INT22_ENERGY37_V31      =
+#include "turner-sint4-rna.dat"
+RNA_INT22_ENERGY37_V23      =
+#include "turner-sint4-rna.dg"
+RNA_INT22_ENTHALPY          =
+#include "turner-sint4-rna.dh"
+RNA_MISCLOOP_ENERGY37_V31   =
+#include "turner-miscloop-rna.dat"
+RNA_MISCLOOP_ENERGY37_V23   =
+#include "turner-miscloop-rna.dg"
+RNA_MISCLOOP_ENTHALPY       =
+#include "turner-miscloop-rna.dh"
+
+// files for dangling ends
+RNA_DANGLE_ENERGY37_V31     =
+#include "turner-dangle-rna.dat"
+RNA_DANGLE_ENERGY37_V23     =
+#include "turner-dangle-rna.dg"
+RNA_DANGLE_ENTHALPY         =
+#include "turner-dangle-rna.dh"
+
+
+// DNA thermodynamic parameters
+// files for stacked pairs
+DNA_STACK_ENERGY37      =
+#include "mathews-stack-dna.dat"
+DNA_STACK_ENTHALPY      =
+#include "mathews-stack-dna.dh"
+
+// files for hairpin loops
+DNA_LOOP_ENERGY37       =
+#include "mathews-loop-dna.dat"
+DNA_LOOP_ENTHALPY       =
+#include "mathews-loop-dna.dh"
+DNA_TRILOOP_ENERGY37    =
+#include "mathews-triloop-dna.dat"
+DNA_TRILOOP_ENTHALPY    =
+#include "mathews-triloop-dna.dh"
+DNA_TLOOP_ENERGY37      =
+#include "mathews-tloop-dna.dat"
+DNA_TLOOP_ENTHALPY      =
+#include "mathews-tloop-dna.dh"
+DNA_TSTACKH_ENERGY37    =
+#include "mathews-tstackh-dna.dat"
+DNA_TSTACKH_ENTHALPY    =
+#include "mathews-tstackh-dna.dh"
+
+// files for intednal loops or bulges
+DNA_TSTACKI_ENERGY37    =
+#include "mathews-tstacki-dna.dat"
+DNA_TSTACKI_ENTHALPY    =
+#include "mathews-tstacki-dna.dh"
+DNA_INT11_ENERGY37      =
+#include "mathews-sint2-dna.dat"
+DNA_INT11_ENTHALPY      =
+#include "mathews-sint2-dna.dh"
+DNA_INT21_ENERGY37      =
+#include "mathews-asint1x2-dna.dat"
+DNA_INT21_ENTHALPY      =
+#include "mathews-asint1x2-dna.dh"
+DNA_INT22_ENERGY37      =
+#include "mathews-sint4-dna.dat"
+DNA_INT22_ENTHALPY      =
+#include "mathews-sint4-dna.dh"
+DNA_MISCLOOP_ENERGY37   =
+#include "mathews-miscloop-dna.dat"
+DNA_MISCLOOP_ENTHALPY   =
+#include "mathews-miscloop-dna.dh"
+
+// files for dangling ends
+DNA_DANGLE_ENERGY37     =
+#include "mathews-dangle-dna.dat"
+DNA_DANGLE_ENTHALPY     =
+#include "mathews-dangle-dna.dh"
---
+++ dat/pkenergy.conf.h
@@ -0,0 +1,46 @@
+// configuration file
+
+// Pseudoknot Energy Model - Dirks and Pierce
+RNA_PKMODEL_DP =
+#include "pkmodelDP.dat"
+DNA_PKMODEL_DP =
+#include "pkmodelDP.dat"
+
+// Pseudoknot Energy Model - Rivas and Eddy
+RNA_PKMODEL_RE =
+#include "pkmodelRE.dat"
+DNA_PKMODEL_RE =
+#include "pkmodelRE.dat"
+
+// Pseudoknot Energy Model - Cao & Chen 2006
+RNA_PKMODEL_CC2006 =
+#include "pkmodelCC2006.dat"
+DNA_PKMODEL_CC2006 =
+#include "pkmodelCC2006.dat"
+
+// Coaxial Stacking - Walter et al 1999
+RNA_COAXSTACK_F_A_ENERGY37 =
+#include "walter-coaxstack-rna.dat"
+DNA_COAXSTACK_F_A_ENERGY37 =
+#include "walter-coaxstack-rna.dat"
+
+// Coaxial Stacking - Mathews et al 1999 and 2007
+// Flush Stacking
+RNA_COAXSTACK_F_B_ENERGY37 =
+#include "coaxial.dat"
+DNA_COAXSTACK_F_B_ENERGY37 =
+#include "coaxial.dat"
+
+// Coaxial Stacking - Mathews et al 1999 and 2007
+// Mismatch Stacking, Continuous Backbone
+RNA_COAXSTACK_M1_ENERGY37 =
+#include "tstackcoax.dat"
+DNA_COAXSTACK_M1_ENERGY37 =
+#include "tstackcoax.dat"
+
+// Coaxial Stacking - Mathews et al 1999 and 2007
+// Mismatch Stacking, Non-Continuous Backbone
+RNA_COAXSTACK_M2_ENERGY37 =
+#include "coaxstack.dat"
+DNA_COAXSTACK_M2_ENERGY37 =
+#include "coaxstack.dat"
