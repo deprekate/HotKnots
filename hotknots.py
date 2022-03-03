2c93c2a0edb9e33b423af38f58522d7443095472
deprekate <deprekate@gmail.com>
2022-03-03 10:57:34 -0800

    Fixed script

--- hotknots.py
+++ hotknots.py
@@ -6,6 +6,7 @@ import argparse
 from argparse import RawTextHelpFormatter
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL) 
+import fileinput
 
 sys.path.pop(0)
 from hotknots import hotknots as hk
@@ -24,7 +25,7 @@ def is_valid_file(x):
 if __name__ == '__main__':
 	usage = '%s [-opt1, [-opt2, ...]] infile' % __file__
 	parser = argparse.ArgumentParser(description='', formatter_class=RawTextHelpFormatter, usage=usage)
-	parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+#	parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 	#parser.add_argument('infile', type=is_valid_file, help='input file')
 	parser.add_argument('-o', '--outfile', action="store", default=sys.stdout, type=argparse.FileType('w'), help='where to write output [stdout]')
 	parser.add_argument('-m', '--model', type=str, default='DP', choices=['DP', 'CC', 'RE'], help='The model to use [DP]')
@@ -37,9 +38,9 @@ if __name__ == '__main__':
 # initialize everything first
 params = os.path.dirname(hk.__file__)
 hk.initialize( args.model, os.path.join(params,"parameters_DP09.txt") , os.path.join(params,"multirnafold.conf"), os.path.join(params,"pkenergy.conf") )
-
+for line in fileinput.input():
+#for line in args.infile:
 # then run each sequence through
-for line in args.infile:
 	print(line.rstrip())
 	seq,mfe = hk.fold( line.rstrip().upper() , args.model )
 	print(seq, mfe)
